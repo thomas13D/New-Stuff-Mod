3ffbd03552ab2f9ad577d3d3381b15c4c8a9b494
thomas13D <TRN13D@gmail.com>
2013-07-27 20:33:02 -0400

    Revert "Ignor Commit"
    This reverts commit 27de7b78fa79ff4e1796e641055c1ee36d2c469d.

--- new_stuff/client/ClientProxyNew_Stuff.java
+++ new_stuff/client/ClientProxyNew_Stuff.java
@@ -1,29 +1,18 @@
 package mods.new_stuff.client;
 
-import net.minecraft.client.renderer.entity.Render;
-import net.minecraft.entity.Entity;
 import net.minecraft.item.ItemStack;
-import net.minecraft.src.ModLoader;
 import net.minecraftforge.common.MinecraftForge;
 import cpw.mods.fml.client.registry.RenderingRegistry;
-import cpw.mods.fml.relauncher.Side;
-import cpw.mods.fml.relauncher.SideOnly;
 import mods.new_stuff.common.CommonProxyNew_Stuff;
 import mods.new_stuff.explosive.EntityPrimedNuclearExplosive;
-import mods.new_stuff.explosive.RenderPrimedNuclearExplosive;
 
 public class ClientProxyNew_Stuff extends CommonProxyNew_Stuff{
-	
 	@Override
-	public void registerRenderInformation(){
-		RenderingRegistry.registerEntityRenderingHandler(EntityPrimedNuclearExplosive.class, new RenderPrimedNuclearExplosive());
-	}
-	
-	  public static int addArmor(String armor){
-	    
-	  return ModLoader.addArmor(armor);
-	}
+	public void registerRenders(){
+		
+		}	
+
+		
 	}
 
-            
-		
+
